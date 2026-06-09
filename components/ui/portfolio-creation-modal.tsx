e5b657bc3fa8e52dcbc6d342d03ef0d2189512bd
DYAROMAR <diyaromar2001@github.com>
2026-06-09 15:09:03 +0200

    debug: marqueurs visuels IMPORT-BUILD-976B0A1 et MODAL-CHOICE-V2 pour diagnostic Vercel

--- components/ui/portfolio-creation-modal.tsx
+++ components/ui/portfolio-creation-modal.tsx
@@ -499,6 +499,9 @@ export function PortfolioCreationModal({
           </button>
         </div>
 
+        {/* DIAGNOSTIC MARKER */}
+        <div className="text-[10px] text-yellow-400">MODAL-CHOICE-V2</div>
+
         <AnimatePresence mode="wait">
           {/* Choice: Manual or Import */}
           {mode === "choice" && (
